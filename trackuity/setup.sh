e6a43170742e63c86ca72fd1940596ef533c0235
Jeroen Janssen <jeroen@trackuity.com>
2016-04-03 22:34:46 +0200

    Ensure python symbolic link is set correctly on slaves as well

--- trackuity/setup.sh
+++ trackuity/setup.sh
@@ -3,3 +3,5 @@
 # Fix the symbolic link issues
 echo "Ensuring python 2.7 is used..."
 ln -f -s /usr/bin/python2.7 /etc/alternatives/python
+
+for line in `cat /root/slaves`; do ssh $line 'ln -f -s /usr/bin/python2.7 /etc/alternatives/python'; done
