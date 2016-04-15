c7fa4e29ab4bb15fb66a406aa4fec09df0482874
Jeroen Janssen <jeroen@trackuity.com>
2016-04-15 12:00:58 +0200

    Ensure deps are installed when installing jupyter

--- jupyter/init.sh
+++ jupyter/init.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 yum -y install python27-devel
-easy_install-2.7 ipython==4.0.0
-easy_install-2.7 notebook==4.0.0
-easy_install-2.7 ipywidgets
-easy_install-2.7 jupyter
+easy_install-2.7 pip
+pip install jupyter
 
