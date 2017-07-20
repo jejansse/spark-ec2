0bda9f496a7067758aea45e35d643399d24b8cdb
Jeroen Janssen <jeroen@trackuity.com>
2017-07-20 17:03:18 +0200

    Ensure slaves are updated before proceeding with installs

--- trackuity/setup.sh
+++ trackuity/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+yum -y update
 yum -y install gcc python27-devel
 
 # Fix the symbolic link issues
@@ -7,6 +8,7 @@ echo "Ensuring python 2.7 is used..."
 ln -f -s /usr/bin/python2.7 /etc/alternatives/python
 
 # Fix for some strange machines without python2.7 we sometimes get
+for line in `cat /root/spark-ec2/slaves`; do ssh $line 'yum -y update'; done
 for line in `cat /root/spark-ec2/slaves`; do ssh $line 'yum -y install python27'; done
 for line in `cat /root/spark-ec2/slaves`; do ssh $line 'ln -f -s /usr/bin/python2.7 /etc/alternatives/python'; done
 # Fix for some strange machines we sometimes get
