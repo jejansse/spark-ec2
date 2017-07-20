fdfff3e7009b5f4b4e15fd69a0717faf939ae1cc
Jeroen Janssen <jeroen@trackuity.com>
2017-07-20 12:27:06 +0200

    Fix for some strange AMIs we sometimes get

--- trackuity/setup.sh
+++ trackuity/setup.sh
@@ -6,4 +6,8 @@ yum -y install python27-devel
 echo "Ensuring python 2.7 is used..."
 ln -f -s /usr/bin/python2.7 /etc/alternatives/python
 
+# Fix for some strange machines without python2.7 we sometimes get
+for line in `cat /root/spark-ec2/slaves`; do ssh $line 'yum -y install python27'; done
 for line in `cat /root/spark-ec2/slaves`; do ssh $line 'ln -f -s /usr/bin/python2.7 /etc/alternatives/python'; done
+# Fix for some strange machines we sometimes get
+for line in `cat /root/spark-ec2/slaves`; do ssh $line 'ln -f -s /usr/bin/python /etc/alternatives/python'; done
