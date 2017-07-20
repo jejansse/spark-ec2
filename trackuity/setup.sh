7ba9a1647994aa96ab499ca46d76c5710eef7017
Jeroen Janssen <jeroen@trackuity.com>
2017-07-20 12:29:32 +0200

    Fix for processing deps and buildout failures

--- trackuity/setup.sh
+++ trackuity/setup.sh
@@ -11,3 +11,10 @@ for line in `cat /root/spark-ec2/slaves`; do ssh $line 'yum -y install python27'
 for line in `cat /root/spark-ec2/slaves`; do ssh $line 'ln -f -s /usr/bin/python2.7 /etc/alternatives/python'; done
 # Fix for some strange machines we sometimes get
 for line in `cat /root/spark-ec2/slaves`; do ssh $line 'ln -f -s /usr/bin/python /etc/alternatives/python'; done
+
+# Install some dependencies for processing
+easy_install-2.7 pip
+pip install setuptools==33.1.1
+pip install zc.buildout
+sudo yum install gcc python-devel libev libev-devel
+export LC_CTYPE="en_US.utf8"  # fix for buidout failing on some python libs when locale is set to UTF-8
