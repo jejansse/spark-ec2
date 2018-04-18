dc33b7dac1023fb3baae4ddce69898704cd97abc
Harm Delva <harm@trackuity.com>
2018-04-18 17:55:34 +0200

    Added scientific computing stack
    DATA-2099

--- trackuity/setup.sh
+++ trackuity/setup.sh
@@ -18,4 +18,7 @@ for line in `cat /root/spark-ec2/slaves`; do ssh $line 'ln -f -s /usr/bin/python
 easy_install-2.7 pip
 pip install setuptools==33.1.1
 pip install zc.buildout
+pip install numpy==1.14.2
+pip install scipy==1.0.1
+pip install scikit-learn==0.19.1
 echo 'export LC_CTYPE="en_US.utf8"' >> /root/.bash_profile  # fix for buidout failing on some python libs when locale is set to UTF-8
