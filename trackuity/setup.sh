a7297f45a04cada43931cd00c4410259e22c7885
Harm Delva <harm@trackuity.com>
2018-07-10 11:02:32 +0200

    Add scikit-learn to slaves

--- trackuity/setup.sh
+++ trackuity/setup.sh
@@ -7,10 +7,22 @@ yum -y install gcc python27-devel
 echo "Ensuring python 2.7 is used..."
 ln -f -s /usr/bin/python2.7 /etc/alternatives/python
 
-# Fix for some strange machines without python2.7 we sometimes get
-for line in `cat /root/spark-ec2/slaves`; do ssh $line 'yum -y update'; done
-for line in `cat /root/spark-ec2/slaves`; do ssh $line 'yum -y install python27'; done
-for line in `cat /root/spark-ec2/slaves`; do ssh $line 'ln -f -s /usr/bin/python2.7 /etc/alternatives/python'; done
+for line in `cat /root/spark-ec2/slaves`;
+do
+  # Fix for some strange machines without python2.7 we sometimes get
+  ssh $line 'yum -y update';
+  ssh $line 'yum -y install python27';
+  ssh $line 'ln -f -s /usr/bin/python2.7 /etc/alternatives/python';
+  ssh $line 'ln -f -s /usr/bin/python /etc/alternatives/python';
+
+  # scientific computing stack
+  ssh $line 'easy_install-2.7 pip';
+  ssh $line 'pip install setuptools==33.1.1';
+  ssh $line 'pip install numpy==1.14.2';
+  ssh $line 'pip install scipy==1.0.1';
+  ssh $line 'pip install scikit-learn==0.19.1';
+done
+
 # Fix for some strange machines we sometimes get
 #for line in `cat /root/spark-ec2/slaves`; do ssh $line 'ln -f -s /usr/bin/python /etc/alternatives/python'; done
 
